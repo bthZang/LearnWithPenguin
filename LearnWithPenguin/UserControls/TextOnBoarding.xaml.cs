36df06555cf65443a0facb4eb04ef5333faf578a
ZangCanhCut <bt.hoanggiang@gmail.com>
2022-10-07 05:00:28 +0700

    OnBoarding ver2.2 :  da add duoc UserControl

---
+++ LearnWithPenguin/UserControls/TextOnBoarding.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LearnWithPenguin.UserControl
+{
+    /// <summary>
+    /// Interaction logic for TextOnBoarding.xaml
+    /// </summary>
+    public partial class TextOnBoarding : System.Windows.Controls.UserControl
+    {
+        public TextOnBoarding()
+        {
+            InitializeComponent();
+        }
+    }
+}
