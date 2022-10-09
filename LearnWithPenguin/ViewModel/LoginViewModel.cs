cec1285a5214909a9b81eca31f0e66a31aa7b3a0
ZangCanhCut <bt.hoanggiang@gmail.com>
2022-10-09 14:29:59 +0700

    OnBoarding ver 5 : thay the passwordbox bang textbox

--- LearnWithPenguin/ViewModel/LoginViewModel.cs
+++ LearnWithPenguin/ViewModel/LoginViewModel.cs
@@ -8,6 +8,30 @@ namespace LearnWithPenguin.ViewModel
 {
     public class LoginViewModel : BaseViewModel
     {
-        
+        public string _display = "";
+        public string Display 
+        {
+            get {
+                string a = DisplayOnScreen(_display);
+                Console.WriteLine(a);
+                return a;
+            }
+            set
+            {
+                Console.WriteLine(value);
+                _display = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string  DisplayOnScreen(string dis)
+        {
+            string scr="";
+            for (int i = 0; i < dis.Length; i++)
+            {
+                scr += "˙";
+            }
+            return scr;
+        }
     }
 }
