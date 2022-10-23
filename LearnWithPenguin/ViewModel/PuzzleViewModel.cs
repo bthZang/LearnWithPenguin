e93eb166f2c61b9ce7cb84da7b8a981bba9d7e1a
PhamNguyetQuynh <phamnguyetquynh0307@gmail.com>
2022-10-23 21:37:01 +0700

    edit puzzle view

--- LearnWithPenguin/ViewModel/PuzzleViewModel.cs
+++ LearnWithPenguin/ViewModel/PuzzleViewModel.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
+using LearnWithPenguin.UserControl;
+using LearnWithPenguin.UserControls;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace LearnWithPenguin.ViewModel
 {
     public class PuzzleViewModel: BaseViewModel
     {
+        protected BackViewModel _navigateHome;
+        public BackViewModel NavigateHome
+        {
+            get
+            {
+                return _navigateHome;
+            }
+            set
+            {
+                _navigateHome = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
