e207110d7db427ffd0a37f2560f611b406d464a4
Trần Ngọc Nhật Vy <21520536@gm.uit.edu.vn>
2022-11-19 15:54:54 +0700

    update: switch được uc của game view

--- LearnWithPenguin/ViewModel/GameViewModel.cs
+++ LearnWithPenguin/ViewModel/GameViewModel.cs
@@ -9,12 +9,17 @@ using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Controls;
 using System.Security.Permissions;
+using LearnWithPenguin.UserControls;
 
 namespace LearnWithPenguin.ViewModel
 {
     public class GameViewModel : BaseViewModel
     {
         protected BaseViewModel _navigatetoCoding;
+
+        private System.Windows.Controls.UserControl _GameTurn;
+        public System.Windows.Controls.UserControl GameTurn { get { return _GameTurn; } set { _GameTurn = value; OnPropertyChanged(); } }
+
 
         public BaseViewModel NavigatetoCoding
         {
@@ -45,43 +50,60 @@ namespace LearnWithPenguin.ViewModel
         public ICommand ForwardCommand { get; set; }
         public ICommand BackwardCommand { get; set; }
 
-
         private string _PositionNumber;
         public string PositionNumber { get { return _PositionNumber; } set { _PositionNumber = value; OnPropertyChanged(); } }
 
-        private string _GameTurn;
-        public string GameTurn { get { return _GameTurn; } set { _GameTurn = value; OnPropertyChanged(); } }
-
         public GameViewModel()
         {
-
             PositionNumber = "1";
-            ForwardCommand = new RelayCommand<object>((p) =>
-            {
-                return true;
-            }, (p) =>
+
+            GameTurn = new Game1();
+
+            ForwardCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
                 int getNum = Convert.ToInt32(PositionNumber);
                 if (getNum == 3)
                     goto stopNum;
                 getNum++;
-            stopNum:
+                stopNum:
                 PositionNumber = Convert.ToString(getNum);
+                switch (getNum)
+                {
+                    case 1:
+                        GameTurn = new Game1();
+                        break;
+                    case 2:
+                        GameTurn = new Game2();
+                        break;
+                    case 3:
+                        GameTurn = new Game3();
+                        break;
+                }
             });
 
-            BackwardCommand = new RelayCommand<object>((p) =>
+            BackwardCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
-                return true;
-            }, (p) =>
-            {
+
                 int getNum = Convert.ToInt32(PositionNumber);
                 if (getNum == 1)
                     goto stopNum;
                 getNum--;
             stopNum:
                 PositionNumber = Convert.ToString(getNum);
-            });
 
+                switch (getNum)
+                {
+                    case 1:
+                        GameTurn = new Game1();
+                        break;
+                    case 2:
+                        GameTurn = new Game2();
+                        break;
+                    case 3:
+                        GameTurn = new Game3();
+                        break;
+                }
+            });
         }
     }
 }
