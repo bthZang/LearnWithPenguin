0a8ecd31fcfe661af295a92a0e38ee862fe78715
PhamNguyetQuynh <phamnguyetquynh0307@gmail.com>
2022-11-23 20:12:01 +0700

    quizview1

--- LearnWithPenguin/View/QuizzView1.xaml.cs
+++ LearnWithPenguin/View/QuizzView1.xaml.cs
@@ -20,9 +20,81 @@ namespace LearnWithPenguin.View
     /// </summary>
     public partial class QuizzView1 : System.Windows.Controls.Page
     {
+        List<int> questionNumbers = new List<int>();
+
+        int questionNumber = 0;
+        int i = 0;
+
+
         public QuizzView1()
         {
             InitializeComponent();
+            FillQuestionNumList();
+            StartGame();
         }
+        private void FillQuestionNumList()
+        {
+            for (int i = 1; i <= 50; i++)
+
+                questionNumbers.Add(i);
+        }
+        private void checkAnswer(object sender, RoutedEventArgs e)
+        {
+            Button senderButton = sender as Button;
+            if (senderButton.Tag.ToString() == "1")
+            {
+
+            }
+            if (questionNumber < 0)
+            {
+                questionNumber = 0;
+            }
+            else
+            {
+                questionNumber++;
+            }
+            NextQuestion();
+        }
+        private void NextQuestion()
+        {
+            if (questionNumber < questionNumbers.Count)
+            {
+                i = questionNumbers[questionNumber];
+            }
+            else
+            {
+                RestartGame();
+            }
+            switch (i)
+            {
+                case 1:
+                    //ans1.B = "Answer 1";
+                    ans2.Content = "Answer 2 Correct";
+                    ans3.Content = "Answer 3";
+                    ans4.Content = "Answer 4";
+
+                    ans2.Tag = "1";
+                    break;
+
+            }
+        }
+        private void RestartGame()
+        {
+            questionNumber = -1;
+            i = 0;
+            StartGame();
+        }
+        private void StartGame()
+        {
+
+            var randomList = questionNumbers.OrderBy(a => Guid.NewGuid()).ToList();
+            questionNumbers = randomList;
+            QuestionOrder.Content = null;
+            for (int i = 0; i < questionNumbers.Count; i++)
+            {
+                QuestionOrder.Content += " " + questionNumbers[i].ToString();
+            }
+        }
+
     }
 }
