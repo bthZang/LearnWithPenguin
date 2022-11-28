51b1da1dcaa42c616c8b33edb42eb947e857bbac
PhamNguyetQuynh <phamnguyetquynh0307@gmail.com>
2022-11-28 23:29:52 +0700

    Entity add

---
+++ LearnWithPenguin/Models/DataReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWithPenguin.Models
+{
+    internal class DataReader
+
+    { 
+        static QuizDatabaseEntities quizz = new QuizDatabaseEntities();
+    }
+}
