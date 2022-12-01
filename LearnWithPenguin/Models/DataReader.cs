e6b00f555aaaef32604de4b3d152ba6b86e0d9e6
PhamNguyetQuynh <phamnguyetquynh0307@gmail.com>
2022-12-01 21:12:33 +0700

    update

--- LearnWithPenguin/Models/DataReader.cs
+++ LearnWithPenguin/Models/DataReader.cs
@@ -9,6 +9,23 @@ namespace LearnWithPenguin.Models
     internal class DataReader
 
     { 
-        //static QuizDatabaseEntities quizz = new QuizDatabaseEntities();
+        static Quizz_LearnWithPenguineEntities quizz = new Quizz_LearnWithPenguineEntities();
+
+        static private Byte ConvertAnserToByte(string data)
+        {
+            switch (data)
+            {
+                case "A":
+                    return 1;
+                case "B":
+                    return 2;
+                case "C":
+                    return 3;
+                case "D":
+                    return 4;
+                default:
+                    return 0;
+            }
+        }
     }
 }
