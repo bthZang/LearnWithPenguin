7d5997c82ba1faca804c9daaa189dc5cd4aaabf2
Zanggg <bt.hoanggiang@gmail.com>
2022-12-11 21:27:35 +0700

    fix bug

--- LearnWithPenguin/View/PuzzleView.xaml.cs
+++ LearnWithPenguin/View/PuzzleView.xaml.cs
@@ -25,12 +25,12 @@ namespace LearnWithPenguin.View
         public QuizQuestionAsideViewModel QuestionVM { get; set; }
         public PuzzleView()
         {
-            
+            InitializeComponent();
+
         }
         public PuzzleView(QuizQuestionAsideViewModel questionVM)
         {
             QuestionVM = questionVM;
-            InitializeComponent();
             this.DataContext = QuestionVM;
         }
 
@@ -38,6 +38,12 @@ namespace LearnWithPenguin.View
         {
             QuizQuestionAsideViewModel currentQuestionVM = QuestionVM;
             NavigationService.Navigate(new QuizQuestionAside(new QuizQuestionAsideViewModel(currentQuestionVM)));
+        }
+
+        private void DockPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            QuestionVM = dockpanel.DataContext as QuizQuestionAsideViewModel;
+            this.DataContext = QuestionVM;
         }
     }
 }
