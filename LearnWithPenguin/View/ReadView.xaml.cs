004c29180afb2ec838b4bc9144f391e347908259
fi <nhuphivo@gmail.com>
2022-12-17 05:10:35 +0700

    chua co text effect khi doc

--- LearnWithPenguin/View/ReadView.xaml.cs
+++ LearnWithPenguin/View/ReadView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +23,65 @@ namespace LearnWithPenguin.View
     {
         public ReadView()
         {
+            InitializeComponent();
+        }
+        public int currentLevel = 1;
+
+        private void NextLesson(object sender, RoutedEventArgs e)
+        {
+            if (currentLevel == 50)
+            {
+                currentLevel = 1;
+            }
+            else
+            {
+                currentLevel = currentLevel + 1;
+            }
+            //lesson nanme
+            lessonName.Text = "Bai " + currentLevel;
+
+            //picture name
+            string pathText = "D:\\School\\testWPFfunc\\FullText\\" + currentLevel + ".txt";
+            string text = System.IO.File.ReadAllText(pathText);
+            picName.Text = text;
+
+            //image
+            string pathImage = "D:\\School\\testWPFfunc\\FullImg\\" + currentLevel + ".jpeg";
+            Uri uri = new Uri(pathImage);
+            ImgChange.Source = new BitmapImage(uri);
+
+        }
+
+        private void PrevLesson(object sender, RoutedEventArgs e)
+        {
+            if (currentLevel == 1)
+            {
+                currentLevel = 50;
+            }
+            else
+            {
+                currentLevel = currentLevel - 1;
+            }
+            //lessson name
+            lessonName.Text = "Bai " + currentLevel;
+
+            //picture name
+            string pathText = "D:\\School\\testWPFfunc\\FullText\\" + currentLevel + ".txt";
+            string text = System.IO.File.ReadAllText(pathText);
+            picName.Text = text;
+
+            //picture
+            string path = "D:\\School\\testWPFfunc\\FullImg\\" + currentLevel + ".jpeg";
+            Uri uri = new Uri(path);
+            ImgChange.Source = new BitmapImage(uri);
+        }
+
+        private void ReadText(object sender, RoutedEventArgs e)
+        {
+            string path = "D:\\School\\testWPFfunc\\FullRecord\\" + currentLevel + ".wav";
+            SoundPlayer player = new SoundPlayer(path);
+            player.Load();
+            player.Play();
         }
     }
 }
