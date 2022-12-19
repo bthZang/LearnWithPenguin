9d5a800e9f56340c3d2675ff02e2f492808f9e2e
fi <nhuphivo@gmail.com>
2022-12-20 00:58:04 +0700

    voice recog ok

--- LearnWithPenguin/View/ReadView.xaml.cs
+++ LearnWithPenguin/View/ReadView.xaml.cs
@@ -13,6 +13,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Windows.Forms.AxHost;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Windows.Media.Animation;
+using System.Drawing;
 
 namespace LearnWithPenguin.View
 {
@@ -24,8 +30,11 @@ namespace LearnWithPenguin.View
         public ReadView()
         {
             InitializeComponent();
+            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
         }
         public int currentLevel = 1;
+        public int currentIndex = 0;
+        private Storyboard myStoryboard;
 
         private void NextLesson(object sender, RoutedEventArgs e)
         {
@@ -38,7 +47,7 @@ namespace LearnWithPenguin.View
                 currentLevel = currentLevel + 1;
             }
             //lesson nanme
-            lessonName.Text = "Bai " + currentLevel;
+            lessonName.Text = "Bài " + currentLevel;
 
             //picture name
             string pathText = "D:\\School\\testWPFfunc\\FullText\\" + currentLevel + ".txt";
@@ -76,6 +85,8 @@ namespace LearnWithPenguin.View
             ImgChange.Source = new BitmapImage(uri);
         }
 
+        // example read button
+
         private void ReadText(object sender, RoutedEventArgs e)
         {
             string path = "D:\\School\\testWPFfunc\\FullRecord\\" + currentLevel + ".wav";
@@ -83,5 +94,132 @@ namespace LearnWithPenguin.View
             player.Load();
             player.Play();
         }
+
+        //kid read button
+        private enum State
+        {
+            Accepting = 1,
+            Off = 2,
+        }
+
+        private State RecogState = State.Off;
+        private SpeechRecognitionEngine srecog;
+        private SpeechSynthesizer synth = null;
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitializeRecognizerSynthesizer();
+
+            if (SelectInputDevice())
+            {
+                LoadDictationGrammar();
+            }
+        }
+
+        private void InitializeRecognizerSynthesizer()
+        {
+            var selectedRecognizer = (from o in SpeechRecognitionEngine.InstalledRecognizers()
+                                      where o.Culture.Equals(Thread.CurrentThread.CurrentCulture)
+                                      select o).FirstOrDefault();
+            srecog = new SpeechRecognitionEngine(selectedRecognizer);
+            srecog.SpeechHypothesized += new EventHandler<SpeechHypothesizedEventArgs>(recognizer_SpeechHypothesized);
+            synth = new SpeechSynthesizer();
+        }
+
+        private bool SelectInputDevice()
+        {
+            bool proceedLoading = true;
+            if (IsOscompatible())
+            {
+                try
+                {
+                    srecog.SetInputToDefaultAudioDevice();
+                }
+                catch
+                {
+                    proceedLoading = false;
+                }
+            }
+            else
+                ThreadPool.QueueUserWorkItem(InitSpeechRecogniser);
+            return proceedLoading;
+        }
+
+        private bool IsOscompatible()
+        {
+            OperatingSystem osInfo = Environment.OSVersion;
+            if (osInfo.Version > new Version("6.0"))
+                return true;
+            else
+                return false;
+        }
+
+        private void InitSpeechRecogniser(object o)
+        {
+            srecog.SetInputToDefaultAudioDevice();
+        }
+
+        private void LoadDictationGrammar()
+        {
+            GrammarBuilder grammarBuilder = new GrammarBuilder();
+            grammarBuilder.Append(new Choices("End Dictate"));
+            Grammar commandGrammar = new Grammar(grammarBuilder);
+            commandGrammar.Name = "main command grammar";
+            srecog.LoadGrammar(commandGrammar);
+
+            DictationGrammar dictationGrammar = new DictationGrammar();
+            dictationGrammar.Name = "dictation";
+            srecog.LoadGrammar(dictationGrammar);
+        }
+
+        // change stage button
+        private void KidRead_Click(object sender, RoutedEventArgs e)
+        {
+            switch (RecogState)
+            {
+                case State.Off:
+                    RecogState = State.Accepting;
+                    KidRead.Content = "Stop";
+                    srecog.RecognizeAsync(RecognizeMode.Multiple);
+                    break;
+                case State.Accepting:
+                    RecogState = State.Off;
+                    KidRead.Content = "Start";
+                    srecog.RecognizeAsyncStop();
+                    picName.Foreground = System.Windows.Media.Brushes.White;
+                    break;
+            }
+        }
+        // recognize words
+        private int Hypothesized = 0;
+        private void recognizer_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            Hypothesized++;
+            var myDoubleAnimation = new DoubleAnimation();
+            myDoubleAnimation.From = 1.0;
+            myDoubleAnimation.To = 0.0;
+            myDoubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            myStoryboard = new Storyboard();
+            myStoryboard.Children.Add(myDoubleAnimation);
+            Storyboard.SetTargetName(myDoubleAnimation, picName.Text);
+            Storyboard.SetTargetProperty(myDoubleAnimation, new PropertyPath(picName.Text));
+            picName.Loaded += new RoutedEventHandler(picNameloaded);
+            
+
+        }
+        private void picNameloaded(object sender, RoutedEventArgs e)
+        {
+            myStoryboard.Begin(this);
+        }
+        private void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            
+            if (RecogState == State.Off)
+                return;
+            float accuracy = (float)e.Result.Confidence;
+            string phrase = e.Result.Text;
+        }
+
+
     }
 }
