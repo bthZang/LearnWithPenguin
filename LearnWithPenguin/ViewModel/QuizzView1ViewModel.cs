ac12b48670e562fbabba91f6fbea4d2b6d71469e
PhamNguyetQuynh <phamnguyetquynh0307@gmail.com>
2022-12-19 12:09:08 +0700

    .

--- LearnWithPenguin/ViewModel/QuizzView1ViewModel.cs
+++ LearnWithPenguin/ViewModel/QuizzView1ViewModel.cs
@@ -394,7 +394,6 @@ namespace LearnWithPenguin.ViewModel
             set { }
         }
 
-        private int point;
         public bool Answer()
         {
 
@@ -407,13 +406,91 @@ namespace LearnWithPenguin.ViewModel
                     Color4 = "red";
                     break;
                 case 2:
-
                     Color1 = "red";
                     Color2 = "red";
                     Color3 = "green";
                     Color4 = "red";
                     break;
-
+                case 3:
+                    Color1 = "green";
+                    Color2 = "red";
+                    Color3 = "red";
+                    Color4 = "red";
+                    break;
+                case 4:
+                    Color1 = "red";
+                    Color2 = "red";
+                    Color3 = "green";
+                    Color4 = "red";
+                    break;
+                case 5:
+                    Color1 = "red";
+                    Color2 = "red";
+                    Color3 = "red";
+                    Color4 = "green";
+                    break;
+                case 6:
+                    Color1 = "green";
+                    Color2 = "red";
+                    Color3 = "red";
+                    Color4 = "red";
+                    break;
+                case 7:
+                    Color1 = "red";
+                    Color2 = "red";
+                    Color3 = "green";
+                    Color4 = "red";
+                    break;
+                case 8:
+                    Color1 = "green";
+                    Color2 = "red";
+                    Color3 = "red";
+                    Color4 = "red";
+                    break;
+                case 9:
+                    Color1 = "red";
+                    Color2 = "red";
+                    Color3 = "red";
+                    Color4 = "green";
+                    break;
+                case 10:
+                    Color1 = "red";
+                    Color2 = "green";
+                    Color3 = "red";
+                    Color4 = "red";
+                    break;
+                case 11:
+                    Color1 = "green";
+                    Color2 = "red";
+                    Color3 = "red";
+                    Color4 = "red";
+                    break;
+                case 12:
+                    Color1 = "red";
+                    Color2 = "red";
+                    Color3 = "green";
+                    Color4 = "red";
+                    break;
+                case 13:
+                    Color1 = "red";
+                    Color2 = "green";
+                    Color3 = "red";
+                    Color4 = "red";
+                    break;
+                case 14:
+                    Color1 = "red";
+                    Color2 = "red";
+                    Color3 = "red";
+                    Color4 = "green";
+                    break;
+                case 15:
+                    Color1 = "green";
+                    Color2 = "red";
+                    Color3 = "red";
+                    Color4 = "red";
+                    break;
+                default:
+                    break;
             }
 
             return false;
@@ -438,6 +515,136 @@ namespace LearnWithPenguin.ViewModel
                     Number2 = 6;
                     Number3 = 5;
                     Number4 = 7;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 3:
+                    Number1 = 9;
+                    Number2 = 10;
+                    Number3 = 11;
+                    Number4 = 12;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 4:
+                    Number1 = 15;
+                    Number2 = 14;
+                    Number3 = 13;
+                    Number4 = 16;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 5:
+                    Number1 = 20;
+                    Number2 = 18;
+                    Number3 = 19;
+                    Number4 = 17;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 6:
+                    Number1 = 21;
+                    Number2 = 22;
+                    Number3 = 23;
+                    Number4 = 24;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 7:
+                    Number1 = 27;
+                    Number2 = 26;
+                    Number3 = 25;
+                    Number4 = 28;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 8:
+                    Number1 = 29;
+                    Number2 = 30;
+                    Number3 = 31;
+                    Number4 = 32;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 9:
+                    Number1 = 36;
+                    Number2 = 34;
+                    Number3 = 35;
+                    Number4 = 33;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 10:
+                    Number1 = 38;
+                    Number2 = 37;
+                    Number3 = 39;
+                    Number4 = 40;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 11:
+                    Number1 = 41;
+                    Number2 = 42;
+                    Number3 = 43;
+                    Number4 = 44;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 12:
+                    Number1 = 47;
+                    Number2 = 46;
+                    Number3 = 45;
+                    Number4 = 48;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 13:
+                    Number1 = 50;
+                    Number2 = 49;
+                    Number3 = 51;
+                    Number4 = 52;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 14:
+                    Number1 = 56;
+                    Number2 = 54;
+                    Number3 = 55;
+                    Number4 = 53;
+                    Color1 = "transparent";
+                    Color2 = "transparent";
+                    Color3 = "transparent";
+                    Color4 = "transparent";
+                    break;
+                case 15:
+                    Number1 = 57;
+                    Number2 = 58;
+                    Number3 = 59;
+                    Number4 = 60;
                     Color1 = "transparent";
                     Color2 = "transparent";
                     Color3 = "transparent";
