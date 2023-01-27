08475afbe8abd8fbdae19925dccd35c486eceece
Zanggg <bt.hoanggiang@gmail.com>
2023-01-27 13:28:20 +0700

    dang yen dang lanh

--- LearnWithPenguin/ViewModel/OnBoardingViewModel.cs
+++ LearnWithPenguin/ViewModel/OnBoardingViewModel.cs
@@ -212,7 +212,7 @@ namespace LearnWithPenguin.ViewModel
                     string password = Password;
                     string confirmPassword = ConfirmPassword;
                     string userName = UserName;
-                    
+
 
                     if (password != confirmPassword)
                     {
@@ -249,183 +249,183 @@ namespace LearnWithPenguin.ViewModel
             set { }
         }
 
-        //SNIPPING
+        ////SNIPPING
 
-        private BaseViewModel _snipping;
+        //private BaseViewModel _snipping;
 
-        public BaseViewModel Snipping
-        {
-            get
-            {
-                return _snipping;
-            }
-            set
-            {
-                _snipping = value;
-                OnPropertyChanged();
-            }
-        }
+        //public BaseViewModel Snipping
+        //{
+        //    get
+        //    {
+        //        return _snipping;
+        //    }
+        //    set
+        //    {
+        //        _snipping = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
 
-        public ICommand ShowSnipping
-        {
-            get
-            {
-                return new RelayCommand<object>((p) => { return true; }, (p) =>
-                {
-                    Snipping = new LoadingPanelViewModel();
-                });
-            }
+        //public ICommand ShowSnipping
+        //{
+        //    get
+        //    {
+        //        return new RelayCommand<object>((p) => { return true; }, (p) =>
+        //        {
+        //            Snipping = new LoadingPanelViewModel();
+        //        });
+        //    }
 
-            set { }
-        }
+        //    set { }
+        //}
 
 
-        public ICommand HideSnipping
-        {
-            get
-            {
-                return new RelayCommand<object>((p) => { return true; }, (p) =>
-                {
-                    Snipping = null;
-                });
-            }
+        //public ICommand HideSnipping
+        //{
+        //    get
+        //    {
+        //        return new RelayCommand<object>((p) => { return true; }, (p) =>
+        //        {
+        //            Snipping = null;
+        //        });
+        //    }
 
-            set { }
-        }
+        //    set { }
+        //}
 
-        //snipping
+        ////snipping
 
-        private bool _panelLoading;
-        private string _panelMainMessage = "Main Loading Message";
-        private string _panelSubMessage = "Sub Loading Message";
+        //private bool _panelLoading;
+        //private string _panelMainMessage = "Main Loading Message";
+        //private string _panelSubMessage = "Sub Loading Message";
 
-        /// <summary>
-        /// Occurs when a property value changes.
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
+        ///// <summary>
+        ///// Occurs when a property value changes.
+        ///// </summary>
+        //public event PropertyChangedEventHandler PropertyChanged;
 
-        /// <summary>
-        /// Gets or sets a value indicating whether [panel loading].
-        /// </summary>
-        /// <value>
-        /// <c>true</c> if [panel loading]; otherwise, <c>false</c>.
-        /// </value>
-        public bool PanelLoading
-        {
-            get
-            {
-                return _panelLoading;
-            }
-            set
-            {
-                _panelLoading = value;
-                RaisePropertyChanged("PanelLoading");
-            }
-        }
+        ///// <summary>
+        ///// Gets or sets a value indicating whether [panel loading].
+        ///// </summary>
+        ///// <value>
+        ///// <c>true</c> if [panel loading]; otherwise, <c>false</c>.
+        ///// </value>
+        //public bool PanelLoading
+        //{
+        //    get
+        //    {
+        //        return _panelLoading;
+        //    }
+        //    set
+        //    {
+        //        _panelLoading = value;
+        //        RaisePropertyChanged("PanelLoading");
+        //    }
+        //}
 
-        /// <summary>
-        /// Gets or sets the panel main message.
-        /// </summary>
-        /// <value>The panel main message.</value>
-        public string PanelMainMessage
-        {
-            get
-            {
-                return _panelMainMessage;
-            }
-            set
-            {
-                _panelMainMessage = value;
-                RaisePropertyChanged("PanelMainMessage");
-            }
-        }
+        ///// <summary>
+        ///// Gets or sets the panel main message.
+        ///// </summary>
+        ///// <value>The panel main message.</value>
+        //public string PanelMainMessage
+        //{
+        //    get
+        //    {
+        //        return _panelMainMessage;
+        //    }
+        //    set
+        //    {
+        //        _panelMainMessage = value;
+        //        RaisePropertyChanged("PanelMainMessage");
+        //    }
+        //}
 
-        /// <summary>
-        /// Gets or sets the panel sub message.
-        /// </summary>
-        /// <value>The panel sub message.</value>
-        public string PanelSubMessage
-        {
-            get
-            {
-                return _panelSubMessage;
-            }
-            set
-            {
-                _panelSubMessage = value;
-                RaisePropertyChanged("PanelSubMessage");
-            }
-        }
+        ///// <summary>
+        ///// Gets or sets the panel sub message.
+        ///// </summary>
+        ///// <value>The panel sub message.</value>
+        //public string PanelSubMessage
+        //{
+        //    get
+        //    {
+        //        return _panelSubMessage;
+        //    }
+        //    set
+        //    {
+        //        _panelSubMessage = value;
+        //        RaisePropertyChanged("PanelSubMessage");
+        //    }
+        //}
 
-        /// <summary>
-        /// Gets the panel close command.
-        /// </summary>
-        public ICommand PanelCloseCommand
-        {
-            get
-            {
-                return new DelegateCommand(() =>
-                {
-                    // Your code here.
-                    // You may want to terminate the running thread etc.
-                    PanelLoading = false;
-                });
-            }
-        }
+        ///// <summary>
+        ///// Gets the panel close command.
+        ///// </summary>
+        //public ICommand PanelCloseCommand
+        //{
+        //    get
+        //    {
+        //        return new DelegateCommand(() =>
+        //        {
+        //            // Your code here.
+        //            // You may want to terminate the running thread etc.
+        //            PanelLoading = false;
+        //        });
+        //    }
+        //}
 
-        /// <summary>
-        /// Gets the show panel command.
-        /// </summary>
-        public ICommand ShowPanelCommand
-        {
-            get
-            {
-                return new DelegateCommand(() =>
-                {
-                    PanelLoading = true;
-                });
-            }
-        }
+        ///// <summary>
+        ///// Gets the show panel command.
+        ///// </summary>
+        //public ICommand ShowPanelCommand
+        //{
+        //    get
+        //    {
+        //        return new DelegateCommand(() =>
+        //        {
+        //            PanelLoading = true;
+        //        });
+        //    }
+        //}
 
-        /// <summary>
-        /// Gets the hide panel command.
-        /// </summary>
-        public ICommand HidePanelCommand
-        {
-            get
-            {
-                return new DelegateCommand(() =>
-                {
-                    PanelLoading = false;
-                });
-            }
-        }
+        ///// <summary>
+        ///// Gets the hide panel command.
+        ///// </summary>
+        //public ICommand HidePanelCommand
+        //{
+        //    get
+        //    {
+        //        return new DelegateCommand(() =>
+        //        {
+        //            PanelLoading = false;
+        //        });
+        //    }
+        //}
 
-        /// <summary>
-        /// Gets the change sub message command.
-        /// </summary>
-        public ICommand ChangeSubMessageCommand
-        {
-            get
-            {
-                return new DelegateCommand(() =>
-                {
-                    PanelSubMessage = string.Format("Message: {0}", DateTime.Now);
-                });
-            }
-        }
+        ///// <summary>
+        ///// Gets the change sub message command.
+        ///// </summary>
+        //public ICommand ChangeSubMessageCommand
+        //{
+        //    get
+        //    {
+        //        return new DelegateCommand(() =>
+        //        {
+        //            PanelSubMessage = string.Format("Message: {0}", DateTime.Now);
+        //        });
+        //    }
+        //}
 
-        /// <summary>
-        /// Raises the property changed.
-        /// </summary>
-        /// <param name="propertyName">Name of the property.</param>
-        protected void RaisePropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
+        ///// <summary>
+        ///// Raises the property changed.
+        ///// </summary>
+        ///// <param name="propertyName">Name of the property.</param>
+        //protected void RaisePropertyChanged(string propertyName)
+        //{
+        //    if (PropertyChanged != null)
+        //    {
+        //        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        //    }
+        //}
     }
 
 }
